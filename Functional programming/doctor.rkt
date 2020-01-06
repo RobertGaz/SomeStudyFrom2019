05fc1f84a03285cac53bd730c0b7b806f46bea39
Robert Gazizov <43959043+RobertGaz@users.noreply.github.com>
2020-01-06 07:09:43 +0300

    to the third task

--- Functional programming/doctor.rkt
+++ Functional programming/doctor.rkt
@@ -3,30 +3,87 @@
 
 #lang scheme/base
 
+; структура шаблонов для ключевых слов
+(define structure
+  '( 
+  (
+    (depressed suicide exams university)
+    ( 
+	  (when you feel depressed, go out for ice cream)
+          (depression is a disease that can be treated)
+          (i can help you in mental problems)
+          (speaking about * will make everything easier)
+	)
+  ) 
+  ( 
+    (mother father parents brother sister uncle ant grandma grandpa)
+    (
+	  (tell me more about your * , i want to know all about your *)
+          (why do you feel that way about your * ?)
+          (yeah. i have interesting stories about my * too)
+          (all this family stuff is always funny)
+          )
+  )
+  (
+    (university school lections)
+	(
+	  (your education is important)
+	  (how many time do you spend to learning ?)
+          (i never liked to go to *)
+          (oh i remember the time i was a student...)
+	)
+  )
+  (
+    (girlfriend boyfriend love wife husband)
+	(
+	  (how much time are you together?)
+	  (it is always interesting to listen about relationships)
+          (there was a time it was difficult for me to talk about my *)
+	)
+  )
+  (
+    (work job colleague office corporation)
+	(
+	  (ooh, where do you work)
+	  (tell me some facts about your *)
+          (what is your profession?)
+	)
+  )
+    ))
+
+; список всех ключевых слов
+(define keylist
+   (let loop ((left structure) (result '()))
+     (cond ((null? left) result)
+           (else (loop (cdr left) (append result  (filter (lambda(elem) (not (member elem result))) (caar left)))))))
+)
+
 
 ; основная функция, запускающая "Доктора"
 ; параметр name -- имя пациента
 (define (visit-doctor stopword max_clients)
-  (cond ((= max_clients 0)
-          (print '(i am worked out)))
-  (else      
-  (let ((name (ask-patient-name))) 
-    (cond ((equal? name stopword)
-         (print '(i need to sleep)))
-    (else
-         (printf "Hello, ~a!\n" name)
-         (print '(what seems to be the trouble?))
-         (doctor-driver-loop name '())
-         (visit-doctor name (- max_clients 1))))))
-  
-  ))
+  (let loop ((clients_left max_clients)) 
+          (cond ((= clients_left 0)
+           (print '(i am worked out)))
+          (else      
+           (let ((name (ask-patient-name))) 
+             (cond ((equal? name stopword)
+                    (print '(i need to sleep)))
+                   (else
+                    (printf "Hello, ~a!\n" name)
+                    (print '(what seems to be the trouble?))
+                    (doctor-driver-loop name '())
+                    (loop (- clients_left 1)))))))
+  )
+)
 
 (define (ask-patient-name)
   (print '(Next!))
   (newline)
   (print '(Who are you?))
   (newline)
-  (car (read)))
+  (car (read))
+)
   
 ; цикл диалога Доктора с пациентом
 ; параметр name -- имя пациента
@@ -50,15 +107,17 @@
 ; генерация ответной реплики по user-response -- реплике от пользователя 
 (define (reply user-response previous)
 
-  (let choose () (case (random 3) 
-               ((0) (qualifier-answer user-response)) 
-               ((1) (hedge))
-               ((2) (if (null? previous) (choose) (history-answer previous)))     
-              )
-  )
-
+  (let ((has_keys (have_keys user-response))) 
+        (let choose () (case (random 4) 
+                         ((0) (qualifier-answer user-response)) 
+                         ((1) (hedge))
+                         ((2) (if (null? previous) (choose) (history-answer previous)))   
+                         ((3) (if has_keys (keyword-answer user-response) (choose)))     
+                        )
+        )
+   )
 )
-			
+
 ; 1й способ генерации ответной реплики -- замена лица в реплике пользователя и приписывание к результату нового начала
 (define (qualifier-answer user-response)
         (append (pick-random '((you seem to think that)
@@ -116,7 +175,26 @@
          )
 )
 
-
 ; 3я стратегия - случайный выбор одной из предыдущих фраз пользователя  
 (define (history-answer previous)
-  (append '(earlier you said that) (change-person (pick-random previous))))
+  (append '(earlier you said that) (change-person (pick-random previous)))
+)
+
+; 4я стратегия - выдача реплики в зависимости от ключевого слова
+(define (keyword-answer replic)
+  (let ((keyword (pick-random (filter (lambda (elem) (member elem keylist)) replic))))
+    (map (lambda (elem) (if (equal? elem '*) keyword elem)) (pick-random (templates keyword)))
+  )
+)
+  
+  
+; есть ли в списке lst хотя бы одно ключевое слово из keylist
+(define (have_keys lst)
+  (let loop ((left lst))
+    (cond ((null? left) #f)
+          ((member (car left) keylist) #t)
+          (else (loop (cdr left))))))
+
+; получить список шаблонов для слова word
+(define (templates word)
+  (foldl (lambda (elem result) (append result (cond ((member word (car elem)) (cadr elem)) (else '())))) '() structure))
